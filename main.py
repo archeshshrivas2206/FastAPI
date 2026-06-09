5665b0e96670e8033e10bc1168c2c4141dbb39fc
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-06-09 15:13:45 +0530

    adding new element done

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends,FastAPI
 from models import product
 from database import sessionlocal,engine
 import db_models 
+from sqlalchemy.orm import Session 
 
 app = FastAPI()
 
@@ -21,6 +22,13 @@ products=[
     product(id=2,name='Laptop',quantity=2,price=550000.0,description='Macbook'),   
 ]
 
+def get_db():
+
+    db = sessionlocal()
+    try :
+        yield db
+    finally:
+        db.close()
 
 def init_db():
     db=sessionlocal()
@@ -32,23 +40,42 @@ def init_db():
 
         db.commit()
 init_db()
- 
-@app.get("/product")
-def get_all_product():
-    return products
 
-@app.get("/product/{id}")
-def get_by_id(id:int):
-    for p in products:
-        if p.id==id:
-
-            return products[id-1]
-    return 'Product not found '
 
 @app.post("/product")
-def add_product(productss:product):
-    products.append(productss)
+def add_product(productss:product,db: Session = Depends(get_db)):
+    db.add(db_models.product(**productss.model_dump()))
+    db.commit()
     return productss
+
+
+
+@app.get("/product")
+def get_all_product(db :Session = Depends(get_db)):
+    db_product= db.query(db_models.product).all()
+
+    return db_product
+
+#this method is used when we ar not dealing with db and directly using any data structure 
+# @app.get("/product/{id}")
+# def get_by_id(id:int):
+#     for p in products:
+#         if p.id==id:
+
+    #         return products[id-1]
+    # return 'Product not found'
+
+@app.get("/product/{id}")
+def get_by_id(id:int, db: Session = Depends(get_db)):
+    db_product=db.query(db_models.product).filter(db_models.product.id==id).first()
+
+    if db_product:
+        return db_product
+    
+    return "Product not found "
+    
+
+
 
 @app.put("/product")
 def edit_product(id:int, product3: product):
